a19f4e2296b8bfa5ff9e0ae6d65565ffc5dd15ec
Pritha Sen <prithasen006@gmail.com>
2024-03-13 16:44:57 +0530

    updated the registration functionality.

--- app.js
+++ app.js
@@ -68,3 +68,69 @@ app.listen(port, () => {
   console.log(`App listening at port ${port}`)
 })
 
+// const express = require('express');
+// const session = require('express-session');
+// const passport = require('./config/passport');
+// const flash = require('connect-flash');
+// const mongoose = require('mongoose');
+// const User = require('./models/usermodel');
+
+// const app = express();
+// const port = 3000;
+
+// mongoose
+//   .connect('mongodb+srv://prithasen006:Mymongo07@todoapi.cufbbh8.mongodb.net/node-api?retryWrites=true&w=majority')
+//   .then(() => {
+//     console.log("Connected to MongoDB")
+//   }).catch(() => {
+//     console.log("Error")
+//   })
+// app.use(express.json())
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended: true }));
+// app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
+
+// // Home route
+// app.get('/', (req, res) => {
+//   res.render('pages/home');
+// });
+
+// // Login route
+// app.get('/login', (req, res) => {
+//   res.render('pages/login', { message: req.flash('error') });
+// });
+
+// // Login post route
+// app.post('/login', passport.authenticate('local', {
+//   successRedirect: '/dashboard',
+//   failureRedirect: '/login',
+//   failureFlash: true
+// }));
+
+// // Dashboard route
+// app.get('/dashboard', (req, res) => {
+//   if (req.isAuthenticated()) {
+//     // User is logged in, redirect to their specific home page
+//     res.redirect(`/users/${req.user.username}`);
+//   } else {
+//     // User is not logged in, display error message
+//     req.flash('error', 'You are not a registered user.');
+//     res.redirect('/login');
+//   }
+// });
+
+// // Logout route
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`App listening at port ${port}`)
+// });
+
+
--- config/passport.js
+++ config/passport.js
@@ -30,3 +30,35 @@ passport.deserializeUser((id, done) => {
 
 module.exports = passport;
 
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const User = require('../models/usermodel');
+
+// passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+//   try {
+//     const user = await User.findOne({ email: email });
+//     if (!user) {
+//       return done(null, false, { message: 'Incorrect email.' });
+//     }
+//     const isMatch = await user.validatePassword(password);
+//     if (!isMatch) {
+//       return done(null, false, { message: 'Incorrect password.' });
+//     }
+//     return done(null, user);
+//   } catch (err) {
+//     return done(err);
+//   }
+// }));
+
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     done(err, user);
+//   });
+// });
+
+// module.exports = passport;
+
